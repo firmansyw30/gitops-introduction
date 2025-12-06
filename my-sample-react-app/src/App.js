b019df27fb65761cee6704b9afeeaab4c0f8eef4
Firmansyah Wicaksono <firmansyahwicaksono30@gmail.com>
2025-12-06 15:21:10 +0700

    chore : add confirmation & update the application manifest for argocd

--- my-sample-react-app/src/App.js
+++ my-sample-react-app/src/App.js
@@ -10,6 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>This is example gitops introduction!</p>
+        <p>It's working</p>
         <a
           className="App-link"
           href="https://reactjs.org"
